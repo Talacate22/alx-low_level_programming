ea539d29f64d5d04cc95eec4510c17c5c0608aba
Talabi Joseph <talabijoseph10@gmail.com>
2023-02-17 05:33:07 -0800

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a c program using printf
+ *
+ * Return:0
+*/ 
+int main(void)
+{
+      printf("with proper grammar, but the outcome is piece of line of art,\n");
+      return (0);
+}	
