3529d6c913f74ae73b7c09d3c62220de6b03fa84
Talabi Joseph <talabijoseph10@gmail.com>
2023-02-23 01:07:46 -0800

    correction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * main - Test function for positive or negative
  * Return: 0
@@ -7,10 +5,10 @@
 
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
- positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
 return (0);
 }
