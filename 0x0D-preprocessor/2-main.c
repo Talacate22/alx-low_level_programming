ea13da1594b692244ae73aedf383c970a3b0a78e
Talabi Joseph <talabijoseph10@gmail.com>
2023-03-20 12:44:22 -0700

    second task

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - prints the file name  with a new line
- * Return: 0;
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
  */
-int main (void)
+int ain(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
